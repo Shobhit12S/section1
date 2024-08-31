fe3053fe064cf9a910dcc465f95af58507e7a80f
shobhit12S <kumarshobhitgamaer@gmail.com>
2024-08-31 10:18:19 +0530

    my do sataement

---
+++ javascript/condition.js
@@ -0,0 +1,36 @@
+let age=20;
+if(age<18){
+    console.log("not elegible")
+}
+else{
+    console.log(" elegible")
+}
+//
+let marks=32;
+if(marks>=90){
+    console.log("A+")
+}
+else if(marks>=80){
+    console.log("A")
+}
+else if(marks>=50){
+    console.log("C")
+}
+else{
+    console.log("fail")
+}
+// caracter is vowel
+let a="A"
+if(a=='A'||a=='E'||a=='I'|| a=='O'
+    ||a=='U')
+{
+    console.log("this is vowel")
+}
+//even odd
+let c=43;
+if(c%2==0){
+    console.log("Even")
+}
+else{
+    console.log("odd")
+}
---
+++ javascript/index.js
@@ -0,0 +1,5 @@
+//area of circle
+let r=32;
+area=3.14*r*r
+console.log(area)
+console.log(typeof area)
